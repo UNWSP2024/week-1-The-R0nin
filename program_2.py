3ec4b5a87045501b74c1c01cbe5263589d25f2d4
The-R0nin <2025esteele@isd2144.org>
2024-09-03 12:14:39 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+    print("See a Man dedicated to  his Bussiness? He Will Stand Before Kings; He Will NOt Stand Before Mean Men. Proverbs 22:29")
     
 
 proverbs_22_29()
